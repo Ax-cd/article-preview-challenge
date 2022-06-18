934ce451c7e35f9766ee1be7618958ffb85abe46
Ax-cd <axcd.coder@gmail.com>
2022-06-18 16:02:08 +0200

    update toggle

--- src/app.js
+++ src/app.js
@@ -1,13 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   const shareBtn = document.querySelector(".share-btn");
-  const returnBtn = document.querySelector(".return-btn");
   const sharing = document.querySelector(".share-section");
 
   function showShare() {
     shareBtn.addEventListener("click", function (e) {
-      sharing.classList.toggle("toggle-display");
-    });
-    returnBtn.addEventListener("click", function (e) {
       sharing.classList.toggle("toggle-display");
     });
   }
